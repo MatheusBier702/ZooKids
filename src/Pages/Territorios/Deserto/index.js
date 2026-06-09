04ea06ab5596cc4bd701022fc9d22bf42ff0d93e
Matheus <matheusbfcss1@gmail.com>
2026-06-09 20:18:43 -0300

    Adicionado(Modal, FlatList e Navigation)

--- src/Pages/Territorios/Deserto/index.js
+++ src/Pages/Territorios/Deserto/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import {View, Text, FlatList, Pressable, Modal, StyleSheet,} from "react-native";
 import styles from "./styles";
@@ -72,84 +71,4 @@ export default function Floresta() {
       </Modal>
     </View>
   );
-=======
-import { StatusBar } from "expo-status-bar";
-import {StyleSheet, Text, SafeAreaView, Image, Pressable, ScrollView, Modal, View, SafeAreaViewBase } from "react-native";
-import React, { useState } from "react";
-import styles from "../../../Artico/styles";
-
-export default function Index() {
-    const [escolhaAnimal, setEscolhaAnimal] = useState(0);
-    const [visivel, setVisivel] = useState(false);
-
-    const animais = [
-        {
-            //camelo   
-        },
-        {
-            //feneco
-        },
-        {
-            //escorpiao do deserto
-        },
-        {
-            //cascavel
-        },
-        {
-            //largato de chifres
-        }
-    ];
-
-    const animal = animais[escolhaAnimal];
-
-    function abrirAnimal(animal){
-        setEscolhaAnimal(animal);
-        setVisivel(true);
-    }
-    return(
-        <SafeAreaView>
-            <ScrollView>
-            <Pressable onPress={() => abrirAnimal(0)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>camelo</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(1)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>feneco</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(2)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>escorpiao do deserto</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(3)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>cascavel</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(4)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>largato de chifres</Text>
-            </Pressable>
-            </ScrollView>
-
-            <Modal visible={visivel} animationType="slide">
-
-            //aq vai colocar as informaçoes que vai colocar no modal dos animais    
-            
-            </Modal>
-
-
-
-
-
-        </SafeAreaView>
-
-
-
-
-    )
->>>>>>> 6cc7a83b688ff5e81a8477cee4c3a0c4e55ad226
 }
--- src/Pages/Territorios/Jurassic/index.js
+++ src/Pages/Territorios/Jurassic/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import {View, Text, FlatList, Pressable, Modal, StyleSheet,} from "react-native";
 import styles from "./styles";
@@ -72,84 +71,4 @@ export default function Floresta() {
       </Modal>
     </View>
   );
-=======
-import { StatusBar } from "expo-status-bar";
-import {StyleSheet, Text, SafeAreaView, Image, Pressable, ScrollView, Modal, View, SafeAreaViewBase } from "react-native";
-import React, { useState } from "react";
-import styles from "../../../Artico/styles";
-
-export default function Index() {
-    const [escolhaAnimal, setEscolhaAnimal] = useState(0);
-    const [visivel, setVisivel] = useState(false);
-
-    const animais = [
-        {
-            //braquiossauro
-        },
-        {
-            //estegossauro
-        },
-        {
-            //alossauro
-        },
-        {
-            //pterodactilo
-        },
-        {
-            //dilofossaro
-        }
-    ];
-
-    const animal = animais[escolhaAnimal];
-
-    function abrirAnimal(animal){
-        setEscolhaAnimal(animal);
-        setVisivel(true);
-    }
-    return(
-        <SafeAreaView>
-            <ScrollView>
-            <Pressable onPress={() => abrirAnimal(0)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>braquiossauro</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(1)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>estegossauro</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(2)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>alossauro</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(3)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>pterodactilo</Text>
-            </Pressable>
-
-            <Pressable onPress={() => abrirAnimal(4)}>
-                <Image source={require("")} style={styles.imagem}/>
-                <Text style={styles.nome}>dilofossaro</Text>
-            </Pressable>
-            </ScrollView>
-
-            <Modal visible={visivel} animationType="slide">
-
-            //aq vai colocar as informaçoes que vai colocar no modal dos animais    
-            
-            </Modal>
-
-
-
-
-
-        </SafeAreaView>
-
-
-
-
-    )
->>>>>>> 6cc7a83b688ff5e81a8477cee4c3a0c4e55ad226
 }
